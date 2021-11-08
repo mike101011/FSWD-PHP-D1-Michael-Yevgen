4a77c97ca79ba9a78968b23138be828e3fadb7b1
Michael Zechner <michael-zechner@gmx.at>
2021-11-08 13:43:22 +0100

    cars array

---
+++ Michael/Challenge/cars.php
@@ -0,0 +1,28 @@
+<?php
+$cars = [
+
+    "mercedes" => [
+        "model" => ["Merc-A"],
+        "seats" => [5],
+        "location" => ["Location 1"],
+        "price" => [40]
+    ],
+    "BMW" => [
+        "model" => ["BM-B"],
+        "seats" => [4],
+        "location" => ["Location 2"],
+        "price" => [45]
+    ],
+    "Audi" => [
+        "model" => ["Aud-C"],
+        "seats" => [4],
+        "location" => ["Location 3"],
+        "price" => [50]
+    ],
+    "Volvo" => [
+        "model" => ["Volv-D"],
+        "seats" => [5],
+        "location" => ["Location 4"],
+        "price" => [60]
+    ]
+];
